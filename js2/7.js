3e6fe2d30289994b417726f650996cedde8d1334
KaterynaVoronina11 <132446552+KaterynaVoronina11@users.noreply.github.com>
2023-11-21 01:44:34 +0200

    Add files via upload

---
+++ js2/7.js
@@ -0,0 +1,19 @@
+
+
+
+
+function validateInput() {
+    var apartmentNumberInput = document.getElementById('apartmentNumber');
+    var tooltipText = document.getElementById('tooltipText');
+
+    var apartmentNumber = parseInt(apartmentNumberInput.value);
+
+    if (isNaN(apartmentNumber) || apartmentNumber < 1 || apartmentNumber > 108) {
+        tooltipText.textContent = 'Невірне значення. Введіть число від 1 до 108.';
+    } else {
+        var entrance = Math.ceil(apartmentNumber / 36);
+        var floor = Math.ceil((apartmentNumber % 36) / 4);
+
+        tooltipText.textContent = 'No під’їзду: ' + entrance + '\nПоверх: ' + floor;
+    }
+}
